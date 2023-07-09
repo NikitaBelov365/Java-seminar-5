ea462d9aa483a1fcaeac6385a6cc04f9aefa0dfb
Nikitabelov365 <nikitabelov365@gmail.com>
2023-07-09 13:07:28 +0300

    task 1 solved

--- First.java
+++ First.java
@@ -1,6 +1,20 @@
-public class First {
+import java.util.*;
 
+public class First {
     public static void main(String[] args) {
-        
+        Map<Integer, String> db = new HashMap<>();
+        db.put(123456, "Иванов");
+        db.put(321456, "Васильев");
+        db.put(234561, "Петров");
+        db.put(234432, "Иванов");
+        db.put(654321, "Петрова");
+        db.put(345678, "Иванов");
+
+        for (var i : db.entrySet()) {
+            if (i.getValue().equals("Иванов")) {
+                System.out.println(i.getKey());
+            }
+           
+        }
     }
 }
